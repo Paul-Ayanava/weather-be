f0bba0b53aa18c31c9744d2498a14591b310967f
Ayanava Paul <ayanavapaul1999@gmail.com>
2024-07-10 14:25:15 +0530

    fix: test coverage improvement

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,4 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger';
-
-import { weatherRouter } from "./routes/weatherForcast";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-// Load environment variables
-require('dotenv').config();
-
-const app = express();
-app.use(json());
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",
-      "https://weatherfe.azurewebsites.net"
-    ]
-  })
-)
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use(weatherRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from "./app";
 
 app.listen(80, () => {
   console.log("listening on port 80!");
--- src/routes/weatherForcast.test.ts
+++ src/routes/weatherForcast.test.ts
@@ -1,32 +1,60 @@
-import request from 'supertest';
-import express from 'express';
-import { weatherRouter } from './weatherForcast';
-import { getWeatherData } from '../services/weatherService';
-import { mockedWeatherData } from '../mocks';
+import request from "supertest";
+import {
+  getWeatherData,
+  getCachedWeatherData,
+} from "../services/weatherService";
+import { mockedWeatherData } from "../mocks";
+import { app } from "../app";
 
-jest.mock('../services/WeatherService');
-const mockedGetWeatherData = getWeatherData as jest.MockedFunction<typeof getWeatherData>;
+jest.mock("../services/WeatherService");
+const mockedGetWeatherData = getWeatherData as jest.MockedFunction<
+  typeof getWeatherData
+>;
+const mockCachedWeatherData = getCachedWeatherData as jest.MockedFunction<
+  typeof getCachedWeatherData
+>;
 
-const app = express();
-app.get('/api/weather/:cityId', weatherRouter);
+describe("WeatherController", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-describe('WeatherController', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  test("should return weather data for a city", async () => {
+    mockedGetWeatherData.mockResolvedValue(mockedWeatherData);
 
-    test('should return weather data for a city', async () => {
-        mockedGetWeatherData.mockResolvedValue(mockedWeatherData);
+    const response = await request(app).get("/api/weather/kolkata");
 
-        const response = await request(app).get('/api/weather/kolkata');
+    expect(response.status).toBe(200);
+    expect(response.body.forecast).toBeDefined();
+  });
 
-        expect(response.status).toBe(200);
-        expect(response.body.forecast).toBeDefined();
-    });
+  test("should return 400 if city is not provided", async () => {
+    const response = await request(app).get("/api/weather");
 
-    test('should return 400 if city is not provided', async () => {
-        const response = await request(app).get('/api/weather');
+    expect(response.status).toBe(404);
+  });
+  test("should return value from cache", async () => {
+    mockCachedWeatherData.mockReturnValue(mockedWeatherData);
 
-        expect(response.status).toBe(404);
-    });
+    const response = await request(app).get("/api/weather/kolkata/offline");
+
+    expect(response.status).toBe(200);
+    expect(response.body.forecast).toBeDefined();
+  });
+
+  test("should return error if value does not exist on cache in offline mode", async () => {
+    mockCachedWeatherData.mockReturnValue(null);
+
+    const response = await request(app).get("/api/weather/kolkata/offline");
+
+    expect(response.status).toBe(500);
+  });
+
+  test("should return error if some error occurs", async () => {
+    mockedGetWeatherData.mockRejectedValue(new Error('Something went wrong'));
+
+    const response = await request(app).get("/api/weather/kolkata");
+
+    expect(response.status).toBe(400);
+  });
 });
